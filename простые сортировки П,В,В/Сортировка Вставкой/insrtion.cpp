8b6b36c9fbd6e7a2b912ca640bdd4f9cbb4d47ad
viktorii726 <231184913+viktorii726@users.noreply.github.com>
2026-04-01 20:02:05 +0500

    Add files via upload

---
+++ простые сортировки П,В,В/Сортировка Вставкой/insrtion.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int n = 7;
+    int tmp; 
+
+    int arr[n] = { 5, 2, 4, 6, 0, 1, 3 };
+
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    
+    for (int i = 1; i < n; i++)
+    {
+    
+        tmp = arr[i];
+
+       
+        int j = i - 1;
+
+        
+        while (j >= 0 && arr[j] > tmp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+
+
+            arr[j + 1] = tmp;
+            for (int i = 0; i < n; i++)
+            {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ простые сортировки П,В,В/Сортировка Выбором/viborom.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include<clocale>
+
+using namespace std;
+
+int main()
+{   
+    setlocale(LC_ALL, ("RUS"));
+    const int n = 7;
+    int tmp; 
+    int arr[n] = { 5, 2, 4, 6, 0, 1, 3 };
+
+    cout << "Исходный массив: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+            {
+                min_index = j;
+            }
+        }
+  
+        if (min_index != i)
+        {
+            tmp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = tmp;
+        }    
+
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+       
+    }
+
+   
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
