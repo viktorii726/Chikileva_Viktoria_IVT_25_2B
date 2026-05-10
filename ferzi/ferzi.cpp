3ef7a73515c670026e35fad82ee5f6452ce65f01
viktorii726 <231184913+viktorii726@users.noreply.github.com>
2026-05-10 15:48:03 +0500

    Add files via upload

---
+++ ferzi/ferzi.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+const int size_arr = 8;
+int arr[size_arr][size_arr];
+int attempt = 1;
+
+void show() {
+    for (int i = 0; i < size_arr; i++) {
+        for (int j = 0; j < size_arr; j++) {
+            cout << (arr[i][j] ? "F " : "# ");
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+bool check(int row, int col) {
+    for (int i = 0; i < row; i++) {
+        if (arr[i][col]) {
+            return false;
+        }
+    }
+    for (int i = 1; row - i >= 0 && col - i >= 0; i++) {
+        if (arr[row - i][col - i]) {
+            return false;
+        }
+    }
+    for (int i = 1; row - i >= 0 && col + i < size_arr; i++) {
+        if (arr[row - i][col + i]) {
+            return false;
+        }
+    }
+    return true;
+}
+void game(int row, int n) {
+    setlocale(LC_ALL, "ru");
+    if (row == size_arr) {
+        cout << "Правильная расстановка " << endl << endl;
+        show();
+        attempt++;
+        return;
+    }
+    for (int col = n; col < size_arr && attempt < 2; col++) {
+        if (check(row, col)) {
+            arr[row][col] = 1;
+            show();
+            game(row + 1, 0);
+            if (attempt < 2) {
+                arr[row][col] = 0;
+                show();
+            }
+        }
+    }
+}
+int main() {
+    int n = 1;
+    game(0, n - 1);
+    return 0;
+}
---
+++ Транспонирование Матрицы/Transponir.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int m, n, ss1[100][100], ss2[100][100];
+
+    cout << "Введите кол-во строк:";
+    cin >> n;
+    cout << "Введите кол-во столбцов:";
+    cin >> m;
+    cout << "Введите элементы массива через пробел:";
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> ss1[i - 1][j - 1];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            ss2[j][i] = ss1[i][j];
+        }
+    }
+
+    cout << "Транспонированный массив:" << endl;
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            ss1[i - 1][j - 1] = ss2[i - 1][j - 1];
+            cout << ss1[i - 1][j - 1] << " ";
+        }
+        cout << endl;
+    }
+
+}
+
